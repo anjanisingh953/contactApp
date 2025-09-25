ab1560d8e6846122ccd62030a8134ebed313238f
Anjani Singh <anjanisingh953@gmail.com>
2025-09-25 23:26:39 +0530

    Implemented code minification

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const minifyHTML = require('express-minify-html-terser');
+
 const app = express();
 const contactRoutes = require('./routes/contacts');
 //middleware
@@ -7,6 +9,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static('public')); //set path for static files such as css,js,image files
 app.use(cookieParser());
+app.use(minifyHTML({
+    override:      true,
+    htmlMinifier: {
+        removeComments:            true,
+        collapseWhitespace:        true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes:     true,
+        removeEmptyAttributes:     true,
+        minifyJS:                  true
+    }
+}));
 
 //set view engine
 app.set('view engine','ejs');
